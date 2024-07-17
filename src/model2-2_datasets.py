d909a1ba20a66862407d55a5de129d99d436ad0b
mitsuru6023149 <xnjhq522@yahoo.co.jp>
2024-07-17 15:37:24 +0900

    Add files via upload
    test

---
+++ src/model2-2_datasets.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import torch
+from typing import Tuple
+from termcolor import cprint
+from glob import glob
+
+
+class ThingsMEGDataset(torch.utils.data.Dataset):
+    def __init__(self, split: str, data_dir: str = "data") -> None:
+        super().__init__()
+        assert split in ["train", "val", "test"], f"Invalid split: {split}"
+        
+        self.split = split
+        self.data_dir = data_dir
+        self.num_classes = 1854
+        self.num_samples = len(glob(os.path.join(data_dir, f"{split}_X", "*.npy")))
+
+        # Compute the mean of the first 10% of the data #追加
+        num_mean_samples = max(1, self.num_samples // 10) # Ensure at least one sample#追加 
+        mean_data = [] #追加
+        for i in range(num_mean_samples):#追加 
+            X_path = os.path.join(self.data_dir, f"{self.split}_X", str(i).zfill(5) + ".npy")#追加 
+            mean_data.append(np.load(X_path)) #追加
+
+        self.mean_data = torch.from_numpy(np.mean(mean_data, axis=0))#追加
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def __getitem__(self, i):
+        X_path = os.path.join(self.data_dir, f"{self.split}_X", str(i).zfill(5) + ".npy")
+        X = torch.from_numpy(np.load(X_path))
+        
+        # Subtract the mean of the first 10% of the data #追加
+        X = X - self.mean_data#追加
+        
+        subject_idx_path = os.path.join(self.data_dir, f"{self.split}_subject_idxs", str(i).zfill(5) + ".npy")
+        subject_idx = torch.from_numpy(np.load(subject_idx_path))
+        
+        if self.split in ["train", "val"]:
+            y_path = os.path.join(self.data_dir, f"{self.split}_y", str(i).zfill(5) + ".npy")
+            y = torch.from_numpy(np.load(y_path))
+            
+            return X, y, subject_idx
+        else:
+            return X, subject_idx
+        
+    @property
+    def num_channels(self) -> int:
+        return np.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.npy")).shape[0]
+    
+    @property
+    def seq_len(self) -> int:
+        return np.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.npy")).shape[1]
